05c7ebe15ad22ca884a79ff7b941c4132146912d
ElGranV <ethan.rinaldo971@gmail.com>
2022-08-08 15:59:02 -0400

    remove videos

--- example.py
+++ example.py
@@ -1,6 +1,6 @@
 from modules.tracking import Tracker
 from modules.analysis import detect_area, DEFAULT_NUMBER_OF_AREAS
-from modules.arduino_io import write_arduino, write_read
+#from modules.arduino_io import write_arduino, write_read
 import cv2
 
 
@@ -14,13 +14,13 @@ old_area = 0
 i=0
 while True:
     success, bbox = trk.update(True)
-    if success:
-        area = detect_area(trk.video, bbox)
-        angle = int(((90*area)/DEFAULT_NUMBER_OF_AREAS))
-        if angle != old_angle:
-            print(area)
-            print("arduino :", write_read(angle))
-        old_angle = angle
+    # if success:
+    #     area = detect_area(trk.video, bbox)
+    #     angle = int(((90*area)/DEFAULT_NUMBER_OF_AREAS))
+    #     if angle != old_angle:
+    #         print(area)
+    #         print("arduino :", write_read(angle))
+    #     old_angle = angle
             
 
     if cv2.waitKey(1) & 0xFF == ord('q'): # if press SPACE bar
